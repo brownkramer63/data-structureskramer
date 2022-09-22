36dba9d89a9ded05fa40288c18217a1ad452528e
Kramer Brown <brownkramer63@gmail.com>
2022-09-22 10:36:58 -0500

    sunset view

--- dsa-05-Stacks/src/main/java/SunsetViews.java
+++ dsa-05-Stacks/src/main/java/SunsetViews.java
@@ -7,10 +7,20 @@ public class SunsetViews {
     }
     public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
         // Write your code here.
-        // Write your code here.
-        // Write your code here.
+   Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i <buildings.length ; i++) {
+            while (!stack.isEmpty() && buildings[i]>=buildings[stack.peek()]){
+
+                stack.pop();
+            }
+            stack.push(i);
+
+        }
+//now i have a stack with heights that can see the sunset it will return the indexes of these buildings
 
 
-        return new ArrayList<Integer>();
+//we will return an array list using the stack like so
+        return new ArrayList<Integer>(stack);
     }
 }
