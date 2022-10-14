815393f5713427f4399a1a2c0ab6ffb2063564fb
Kramer Brown <brownkramer63@gmail.com>
2022-10-14 14:20:34 -0500

    queues

--- dsa-07-Queues/src/main/java/com/Queues/CollectionsQueue.java
+++ dsa-07-Queues/src/main/java/com/Queues/CollectionsQueue.java
@@ -1,7 +1,28 @@
 package com.Queues;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CollectionsQueue {
     public static void main(String[] args) {
+MyQueue<Integer> mq = new MyQueue<>();
 
+mq.enqueue(1);
+mq.enqueue(2);
+
+
+        System.out.println(mq.dequeue());
+        Queue<Integer> cq = new LinkedList<>();
+        cq.add(1);
+        cq.add(2);
+        cq.add(3);
+        cq.add(1);
+
+        System.out.println(cq);
+    }
+    public Queue<Integer> reversequeue(Queue<Integer> queue1, int k){
+
+
+return null;
     }
 }
--- dsa-07-Queues/src/main/java/com/Queues/MyQueue.java
+++ dsa-07-Queues/src/main/java/com/Queues/MyQueue.java
@@ -1,34 +1,52 @@
 package com.Queues;
 
-public class MyQueue <T>{
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
 
     QNode<T> front;
     QNode<T> back;
     int size;
 
-    boolean isEmpty(){
-        return front==null;
+    boolean isEmpty() {
+        return front == null;
     }
 
-    T peak(){
+    T peak() {
         return (T) front.value;
     }
 
-    void enqueue(T item){
-        QNode<T> node= new QNode<>(item);
-        if (isEmpty()){
-            front=back=node;
-        }
-        else{
-            back.next= node;
-            back=node;
+    void enqueue(T item) {
+        QNode<T> node = new QNode<>(item);
+        if (isEmpty()) {
+            front = back = node;
+        } else {
+            back.next = node;
+            back = node;
         }
         size++;
     }
 
+    T dequeue() {
+        QNode frontNode;
+        if (isEmpty()) throw new NoSuchElementException();
+//for one element in queue
 
+        if (front == back) {
+            frontNode = front;
+            front = back = null;
+            //now i have more than one element
+        } else {
+            frontNode = front;
+            front = front.next;
+        }
+        size--;
+        return (T) frontNode.value;
+    }
 
-
+    int size() {
+        return size;
+    }
 
 
 }
