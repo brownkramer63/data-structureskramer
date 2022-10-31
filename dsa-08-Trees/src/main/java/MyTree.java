d3b6e2606377852da88f911d522226a8a30ceec6
Kramer Brown <brownkramer63@gmail.com>
2022-10-31 09:55:12 -0500

    trees

--- dsa-08-Trees/src/main/java/MyTree.java
+++ dsa-08-Trees/src/main/java/MyTree.java
@@ -1,34 +1,47 @@
 public class MyTree {
     TNode root;
 
-    public MyTree(){
+    public MyTree() {
 
     }
-    void insert(int value){
-    TNode newNode= new TNode(value);
-    if (root==null){
-        root=newNode;
-        return;
-    }
-    TNode current = root;
-    while (true){
-        if (value<= current.value){
-            if (current.leftChild==null){
-                current.leftChild=newNode;
-                break;
-            }
-           current=current.leftChild;
-            }
-        else {
-            if (current.rightChild==null){
-                current.rightChild=newNode;
-                break;
-            }
-            current=current.rightChild;
+
+    void insert(int value) {
+        TNode newNode = new TNode(value);
+        if (root == null) {
+            root = newNode;
+            return;
         }
+        TNode current = root;
+        while (true) {
+            if (value <= current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = newNode;
+                    break;
+                }
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = newNode;
+                    break;
+                }
+                current = current.rightChild;
+            }
 
+        }
     }
+//preorder transversal of the tree
+    // root left right
+    void preOrderTraversal(TNode root){
+        if (root==null) return;
+        System.out.println(root.value);
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
     }
-
+    void inOrderTraversal(TNode root){
+        if (root==null) return;
+        inOrderTraversal(root.leftChild);
+        System.out.print(root.value+", ");
+        inOrderTraversal(root.rightChild);
+    }
 
 }
