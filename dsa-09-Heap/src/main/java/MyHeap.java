d667610aef8437a5dcbfdb1accf8defeaa144791
Kramer Brown <brownkramer63@gmail.com>
2022-11-18 14:51:20 -0600

    heap

--- dsa-09-Heap/src/main/java/MyHeap.java
+++ dsa-09-Heap/src/main/java/MyHeap.java
@@ -12,4 +12,35 @@ public class MyHeap {
         if (size==0) throw new NoSuchElementException();
         return items[0];
     }
+    public void insert(int value) {
+        if(size==items.length) throw new IllegalStateException();
+        else {
+           items[size++]=value;
+           bubbleUp();
+        }
+    }
+    public void bubbleUp(){
+        //calculate start point/index
+        int index=size-1;
+        // while index >0 %% parent is less than child continue...
+        // 1. swap child and parent
+        // calculate next index = parent index
+        while (index>0 && items[index]>items[parentIndex(index)]){
+            swap (index, parentIndex(index));
+            // assign index with the parent index
+            index=parentIndex(index);//like current=current.next
+        }
+    }
+    public void swap(int first, int second){
+        int temp=items[first];
+        items[first]=items[second];
+        items[second]=temp;
+    }
+    public int parentIndex(int index) { return (index-1)/2;}
+    public void printHeap(){
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i]+ ", ");
+        }
+        System.out.println();
+    }
 }
