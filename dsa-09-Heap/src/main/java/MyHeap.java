cd49f9857c9644c7c0dd65c029466425ae7b5395
Kramer Brown <brownkramer63@gmail.com>
2022-11-18 15:19:00 -0600

    heap

--- dsa-09-Heap/src/main/java/MyHeap.java
+++ dsa-09-Heap/src/main/java/MyHeap.java
@@ -31,12 +31,53 @@ public class MyHeap {
             index=parentIndex(index);//like current=current.next
         }
     }
+
+    public int remove(){
+        if (size==0) throw new NoSuchElementException();
+        else {
+            int result= items[0];
+            items[0]=items[--size];
+            bubbleDown();
+            return result;
+        }
+    }
+    public void bubbleDown(){
+        int index=0;
+        int largerChildIndex;
+        while (index<=size && !isValidParent(index)){
+            largerChildIndex=largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index=largerChildIndex;
+        }
+    }
+    public boolean isValidParent(int index){
+        if (!hasLeftChild(index)) return true;
+        else {
+            boolean isValid=false;
+            if (hasRightChild(index)) {
+                isValid= (items[index]>=items[leftChildIndex(index)]&&items[index]>=items[rightChildIndex(index)]);
+            }
+            return isValid;
+        }
+    }
+
     public void swap(int first, int second){
         int temp=items[first];
         items[first]=items[second];
         items[second]=temp;
     }
     public int parentIndex(int index) { return (index-1)/2;}
+    public boolean hasLeftChild(int index){ return  leftChildIndex(index)<=size;}
+    public boolean hasRightChild(int index){ return  rightChildIndex(index)<=size;}
+    public int leftChildIndex(int index){ return index*2 +1;}
+    public int rightChildIndex(int index){ return index*2 +2;}
+    public int largerChildIndex(int index) {
+        if (!hasLeftChild(index)) return index;
+        else if (!hasRightChild(index)) {
+            return leftChildIndex(index);
+        }
+        return items[leftChildIndex(index)]>items[rightChildIndex(index)] ? leftChildIndex(index):rightChildIndex(index);
+    }
     public void printHeap(){
         for (int i = 0; i < size; i++) {
             System.out.print(items[i]+ ", ");
